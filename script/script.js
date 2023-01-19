53103b79e70a1b63dbb3f1198b94710067cc2c29
ledeuadian <adrianjrfabian@gmail.com>
2023-01-19 08:53:44 +0800

    for def

--- script/script.js
+++ script/script.js
@@ -35,11 +35,44 @@ Order = {name: itemsName, image: itemImage, price: itemPrice};
 for (let i = 0; i <= localStorage.length; i++) {
     number = i;
 }
-    localStorage.setItem("Order"+number, (Order));
+    localStorage.setItem("Order"+number, JSON.stringify(Order));
 };
 
-function displayToCart(){
-let orderGet = localStorage.getItem('Order1');
-console.log(orderGet.Order);
+
+//Display to cart
+
+const cartContent = document.querySelector("#cart-content");
+
+
+function displayToCart(){ 
+
+productMarkup = '';
+
+for (let i = 1; i <= localStorage.length; i++) {
+   OrderNum = 'Order'+i;
+   let orderGet = localStorage.getItem('Order1');
+   let pullName = JSON.parse(orderGet).name;
+   let pullImage = JSON.parse(orderGet).image;
+   let pullPrice = JSON.parse(orderGet).price;
+
+   productMarkup +=
+`
+  <tr>
+  <td>
+    <img src="${pullImage}" alt="${pullName}" width="120" style="width:6rem; border:1px solid black; border-radius:30px">
+  </td>
+  <td>
+    ${pullName}
+  </td>
+  <td>₱${pullPrice}</td>
+  <td><input type="number" style="width:40px;"></td>
+  <td><button type="button" class="bg-danger" style="width:30px;"> X </button></td>
+  </tr>
+`;
+}
+cartContent.querySelector("tbody").innerHTML = productMarkup;
 
 };
+
+
+
